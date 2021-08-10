4f1fca946e191f0d65563b2cb3a22ab382208c83
Gihyeon <ken29511@gmail.com>
2021-08-10 23:46:40 +0900

    PRACTICE:while-for

---
+++ while-for/app.js
@@ -0,0 +1,16 @@
+function whileExample() {
+  let i = 0;
+  while (i < 100) {
+    console.log(i + 1);
+    i++;
+  }
+}
+
+function forExample() {
+  for (let i = 0; i < 100; i++) {
+    console.log(i + 1);
+  }
+}
+
+whileExample();
+forExample();
