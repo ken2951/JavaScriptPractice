69fa4b04de67630aa7e9cf5649e0a922ff40f139
Gihyeon KIM <ken29511@gmail.com>
2021-09-01 23:10:09 +0900

    add: counter

--- counter/app.js
+++ counter/app.js
@@ -1,41 +1,31 @@
-/*
-# Counter
-
-> 간단한 카운터를 만들어보자.
-
-![카운터](../../assets/screenshots/counter.png)
-
-위의 이미지처럼 버튼 3개가 존재하고 버튼을 누름에 따라서 해당 결과값이 표시된다.
-
-버튼 기능
-
-- Increase : 표시되는 결과값이 1 증가한다.
-- Decrease : 표시되는 결과값이 1 감소한다.
-- Reset : 초기화 버튼
- */
-
 const $result = document.querySelector("#result");
-let result = Number($result.textContent);
+const $increase = document.querySelector("#increase");
+const $reset = document.querySelector("#reset");
+const $decrease = document.querySelector("#decrease");
 
 const onClickButton = (event) => {
+  let result = Number($result.textContent);
   switch (event.target.textContent) {
-    case "+":
+    case "+": {
       result += 1;
       $result.textContent = result;
       break;
-    case "reset":
+    }
+    case "reset": {
       result = 0;
       $result.textContent = result;
       break;
-    case "-":
+    }
+    case "-": {
       result -= 1;
       $result.textContent = result;
       break;
+    }
     default:
       break;
   }
 };
 
-document.querySelector("#increase").addEventListener("click", onClickButton);
-document.querySelector("#reset").addEventListener("click", onClickButton);
-document.querySelector("#decrease").addEventListener("click", onClickButton);
+$increase.addEventListener("click", onClickButton);
+$reset.addEventListener("click", onClickButton);
+$decrease.addEventListener("click", onClickButton);
