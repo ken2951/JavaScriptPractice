c05fbf6ee8fbaab01dfb87f437dcc5176869721d
Gihyeon KIM <ken29511@gmail.com>
2021-09-07 23:27:37 +0900

    fix: 2_background_flipper 버튼 클릭시 버튼색 및 글자색 변경되도록 수정

--- background_flipper/app.js
+++ background_flipper/app.js
@@ -2,14 +2,27 @@ const [$tomato, $dodgerblue, $hotpink, $yellowgreen, $chocolate, $reset] =
   document.querySelectorAll("button");
 const $background = document.querySelector(".background");
 
+let clicked;
+
 const onClickButton = (event) => {
-  let color = event.target.textContent;
+  const click = event.target;
+  const color = click.textContent;
+
+  if (clicked !== undefined) {
+    clicked.style.color = "white";
+    clicked.style.background = clicked.textContent;
+  }
 
   if (color === "RESET") {
     $background.style.background = "white";
-  } else {
-    $background.style.background = color;
+    return;
   }
+
+  $background.style.background = color;
+  click.style.color = color;
+  click.style.background = "white";
+
+  clicked = click;
 };
 
 $tomato.addEventListener("click", onClickButton);
