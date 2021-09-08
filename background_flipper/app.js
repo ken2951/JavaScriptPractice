e32eafdfb0f321e48a111c4a434bb6efa341cf0c
Gihyeon KIM <ken29511@gmail.com>
2021-09-09 00:12:47 +0900

    refactoring: 변수명 변경 등 자잘한 수정

--- background_flipper/app.js
+++ background_flipper/app.js
@@ -1,27 +1,29 @@
 const $buttons = document.querySelectorAll("button");
 const $background = document.querySelector(".background");
 
-let clicked;
+let tempTarget;
 
 const onClickButton = (event) => {
   const target = event.target;
   const color = target.textContent;
+  const RESET = "RESET";
+  const resetColor = "white";
 
-  if (clicked) {
-    clicked.style.color = "white";
-    clicked.style.background = clicked.textContent;
+  if (tempTarget) {
+    tempTarget.style.color = resetColor;
+    tempTarget.style.background = tempTarget.textContent;
   }
 
-  if (color === "RESET") {
-    $background.style.background = "white";
+  if (color === RESET) {
+    $background.style.background = resetColor;
     return;
   }
 
   $background.style.background = color;
   target.style.color = color;
-  target.style.background = "white";
+  target.style.background = resetColor;
 
-  clicked = target;
+  tempTarget = target;
 };
 
 $buttons.forEach((button) => {
