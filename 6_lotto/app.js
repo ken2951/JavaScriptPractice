394376423f69e878e9128106d30e87f1fe46b0bf
Gihyeon KIM <ken29511@gmail.com>
2021-10-14 00:42:05 +0900

    add: function getLottoNumber, checkLottoNumber

--- 6_lotto/app.js
+++ 6_lotto/app.js
@@ -1,29 +1,69 @@
-//6개의 숫자를 입력
-//로또 랜덤 숫자를 6개 + 보너스 숫자 1개 생성
-//1등 6개 일치
-//2등 5개 일치 보너스 숫자 일치
-//3등 5개 일치
-//4등 4개 일치
-//5등 3개 일치
 const $input = document.querySelectorAll("input");
 const $button = document.querySelector("button");
+const $result = document.querySelector("#result");
+
+//테스트용
+// const lottoArray = getLottoNumber();
+// console.log(lottoArray);
 
 function handleClickButton() {
-  let array = [];
+  const inputArray = getInputNumber();
+  const lottoArray = getLottoNumber();
+  const result = checkLottoNumber(inputArray, lottoArray);
+  $result.textContent = result;
+}
+
+function getInputNumber() {
+  let inputArray = [];
   $input.forEach((input) => {
-    array.push(input.value);
+    inputArray.push(input.value);
   });
-  console.log(array);
-  const num1 = getLottoNumber();
-  console.log(num1);
+  return inputArray;
 }
 
 function getLottoNumber() {
-  //1~45의 랜덤 숫자 7개
   const min = 1;
   const max = 45;
-  let num1 = Math.floor(Math.random() * (max + 1 - min) + 1);
-  return num1;
+  let lottoArray = [];
+  for (let i = 0; i < 7; i++) {
+    let randomNumber;
+    do {
+      randomNumber = Math.floor(Math.random() * (max + 1 - min) + 1);
+    } while (lottoArray.includes(randomNumber));
+    lottoArray.push(randomNumber);
+  }
+  return lottoArray;
+}
+
+function checkLottoNumber(inputArray, lottoArray) {
+  const bonusNumber = String(lottoArray.pop());
+  let resultArray = [];
+  let result;
+
+  lottoArray.forEach((lottoNumber) => {
+    if (inputArray.includes(String(lottoNumber))) {
+      resultArray.push(lottoNumber);
+    }
+  });
+
+  if (resultArray.length < 3) {
+    return (result = "다음 기회에");
+  }
+  if (resultArray.length === 3) {
+    return (result = "5등");
+  }
+  if (resultArray.length === 4) {
+    return (result = "4등");
+  }
+  if (resultArray.length === 5) {
+    if (inputArray.includes(bonusNumber)) {
+      return (result = "2등");
+    }
+    return (result = "3등");
+  }
+  if (resultArray.length === 6) {
+    return (result = "1등");
+  }
 }
 
 $button.addEventListener("click", handleClickButton);
